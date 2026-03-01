7f924d4a715aa591a5823a517cc33ac3e3d9775c
rahma hassan <musa.rahama2020@uniabuja.edu.ng>
2026-03-01 23:48:39 +0100

    CORS TO USE FRONTEND URL

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: "https://route-pool-frontend-2-0.onrender.com",
     credentials: true,
   }),
 );
